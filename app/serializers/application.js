965df108bc1a9d799a03c193388e04776028d5c6
Janusz Mordarski <janusz.m@gmail.com>
2017-01-23 22:45:01 +0100

    Add serializer for Rails API (underscore keys)

---
+++ app/serializers/application.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+let underscore = Ember.String.underscore;
+
+export default DS.JSONAPISerializer.extend({
+  keyForAttribute: function(attr) {
+    return underscore(attr);
+  },
+
+  keyForRelationship: function(rawKey) {
+    return underscore(rawKey);
+  }
+});
