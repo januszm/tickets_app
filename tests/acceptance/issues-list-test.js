d8b108b5bac4c3341152689289a19eb0695fe8ba
Janusz Mordarski <janusz.m@gmail.com>
2017-01-23 23:46:47 +0100

    Redirect to Index after logout and improve tests

--- tests/acceptance/issues-list-test.js
+++ tests/acceptance/issues-list-test.js
@@ -1,11 +1,23 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'tickets-app/tests/helpers/module-for-acceptance';
 
+import {
+  authenticateSession,
+  invalidateSession
+} from 'tickets-app/tests/helpers/ember-simple-auth';
+
 moduleForAcceptance('Acceptance | list issues');
 
 test('visiting /issues', function(assert) {
-  visit('/issues');
 
+  invalidateSession(this.application);
+  visit('/issues');
+  andThen(function() {
+    assert.equal(currentURL(), '/login');
+  });
+
+  authenticateSession(this.application);
+  visit('/issues');
   andThen(function() {
     assert.equal(currentURL(), '/issues');
   });
