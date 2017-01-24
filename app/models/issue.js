93b7989fd2f033377c7e2d2c984cbde6d5e5ff96
Janusz Mordarski <janusz.m@gmail.com>
2017-01-24 15:23:06 +0100

    Update association attributes on Issue model for consistency

--- app/models/issue.js
+++ app/models/issue.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   description: DS.attr(),
-  creator_id: DS.attr(),
-  assignee_id: DS.attr()
+  creatorId: DS.attr(),
+  assigneeId: DS.attr(),
+  status: DS.attr()
 });
