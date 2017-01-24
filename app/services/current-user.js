8dc9eb20164388185dfa779c3528940ce7db1fe3
Janusz Mordarski <janusz.m@gmail.com>
2017-01-24 16:59:31 +0100

    Add current User service loaded in application route

---
+++ app/services/current-user.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+const { service } = Ember.inject;
+const { RSVP, isEmpty } = Ember;
+
+export default Ember.Service.extend({
+  session: service('session'),
+  store: service(),
+
+  currentUserId() {
+    let jwt = this.get('session.data.authenticated.jwt');
+    return JSON.parse(atob(jwt.split('.')[1])).sub;
+  },
+  load() {
+    if (this.get('session.isAuthenticated')) {
+      return this.get('store').findRecord('user', this.currentUserId())
+        .then(user => this.set('user', user));
+    } else {
+      return Ember.RSVP.resolve();
+    }
+  }
+});
