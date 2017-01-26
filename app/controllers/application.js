0be0da6db2e168fe41bd586b53c6bccdc0eea85b
Janusz Mordarski <janusz.m@gmail.com>
2017-01-27 00:34:02 +0100

    Refactored currentUser usage

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
+  currentUser: Ember.inject.service(),
+  getCurrentUser: Ember.computed(function() {
+    return this.get('currentUser.user');
+  }),
 
   actions: {
     invalidateSession() {
