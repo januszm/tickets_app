0ce80e167356a1bbffb3ebc9b0c9ae63aadd1444
Janusz Mordarski <janusz.m@gmail.com>
2017-01-27 00:39:02 +0100

    Add computed issue status properties

--- app/models/issue.js
+++ app/models/issue.js
@@ -1,9 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr(),
   description: DS.attr(),
   creatorId: DS.attr(),
   assigneeId: DS.attr(),
-  status: DS.attr()
+  status: DS.attr(),
+  isOpen: Ember.computed('status', function() {
+    return this.get('status') === 'opened';
+  }),
+  isClosed: Ember.computed('status', function() {
+    return this.get('status') === 'closed';
+  }),
 });
